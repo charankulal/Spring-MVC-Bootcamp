e0ea6996bb8ed77849cfd89cf94783a31161b504
Charan <charankulal0241@gmail.com>
2024-04-28 22:31:57 +0530

    Created suffix and prefix for the view files for the security purpose.

--- src/main/java/com/charan/spring_mvc/HomeController.java
+++ src/main/java/com/charan/spring_mvc/HomeController.java
@@ -5,13 +5,14 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String home()
     {
-        return "index.jsp";
+        return "index";
     }
 
 //    @RequestMapping("/add")
@@ -25,11 +26,21 @@ public class HomeController {
 //        return "result.jsp";
 //    }
 
+//    @RequestMapping("/add")
+//    public String add(@RequestParam("num1") int i ,@RequestParam("num2") int j, HttpSession session)
+//    {
+//        int num3=i+j;
+//        session.setAttribute("num3",num3);
+//        return "result.jsp";
+//    }
+
     @RequestMapping("/add")
-    public String add(@RequestParam("num1") int i ,@RequestParam("num2") int j, HttpSession session)
+    public ModelAndView add(@RequestParam("num1") int i ,@RequestParam("num2") int j, HttpSession session)
     {
+        ModelAndView mv= new ModelAndView();
         int num3=i+j;
-        session.setAttribute("num3",num3);
-        return "result.jsp";
+        mv.setViewName("result");
+        mv.addObject("num3",num3);
+        return mv;
     }
 }
